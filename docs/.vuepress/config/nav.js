fb15c636643244afff54e5d2a479e75191896c72
1425821290 <1425821290@qq.com>
2023-05-16 22:47:56 +0800

    更新

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,16 +1,12 @@
 module.exports = [
-    { text: '主页', link: '/', icon: 'reco-home' },
-    { 
-        text: '前端', link: '/前端/', icon: 'reco-home',
+    { text: "主页", link: "/", icon: "reco-home" },
+    {
+        text: "前端",
+        link: "/前端/",
+        icon: "reco-home",
         items: [
-            { 
-                text: '面试相关', 
-                items: [
-                    {
-                        text: '面试', link: '/前端/面试/js'
-                    } 
-                ] 
-            },
-        ] 
-    }
-]
+            { text: "面试相关", link: "/前端/面试/js" },
+            { text: "Vue2", link: "/前端/Vue/Vue2基础" },
+        ],
+    },
+];
